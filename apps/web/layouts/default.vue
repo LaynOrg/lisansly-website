56f13b630e1c2bd85eaffac540a9c18c2372da63
yali <tangozyusufali@gmail.com>
2023-12-09 22:13:45 +0300

    feat(web): add main to layout, wrap notification bar and header with fixed div

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-    <NotificationBar />
-    <Header />
-    <slot />
+    <div class="fixed top-0 left-0 w-full">
+        <NotificationBar />
+        <Header />
+    </div>
+    <main class="min-h-screen pt-28">
+        <slot />
+    </main>
 </template>
