bef2a3aa57e3561b4039d6f6d8ba527a16ad67ab
yali <tangozyusufali@gmail.com>
2023-12-09 14:40:43 +0300

    feat(web): header

--- apps/web/i18n.config.ts
+++ apps/web/i18n.config.ts
@@ -2,7 +2,15 @@ export default defineI18nConfig(() => ({
   legacy: false,
   locale: "tr",
   messages: {
-    tr: {},
-    en: {},
+    tr: {
+      header: {
+        links: ["Anasayfa", "Dokümantasyon", "Fiyatlandırma", "Panel"],
+      },
+    },
+    en: {
+      header: {
+        links: ["Home", "Documantation", "Pricing", "Dashboard"],
+      },
+    },
   },
 }));
