2256e51cd6a2810138900faec79f7e3b8a3967cd
yali <tangozyusufali@gmail.com>
2023-12-09 17:08:55 +0300

    feat(web): hero header

--- apps/web/i18n.config.ts
+++ apps/web/i18n.config.ts
@@ -7,12 +7,32 @@ export default defineI18nConfig(() => ({
         links: ["Anasayfa", "Dokümantasyon", "Fiyatlandırma", "Panel"],
       },
       notificationBarLabel: "You can change the language of the site.",
+      home: {
+        heroHeader: {
+          title:
+            "Lisansly ile uygulamanıza Lisans veya Kimlik doğrulama ekleyin",
+          description:
+            "Uygulamalarınıza kimlik doğrulama eklemenin kolay ve uygun maliyetli yolu.",
+          learnMore: "Öğrenmeye Başlayın",
+          start: "Ücretsiz Plan ile Başlayın",
+        },
+      },
     },
     en: {
       header: {
         links: ["Home", "Documantation", "Pricing", "Dashboard"],
       },
       notificationBarLabel: "Sitenin dilini değiştirebilirsiniz.",
+      home: {
+        heroHeader: {
+          title:
+            "Add License or Authentication to your application with Lisansly",
+          description:
+            "An easy and inexpensive way to add authentication to your application.",
+          learnMore: "Start Learning",
+          start: "Start with Free Plan",
+        },
+      },
     },
   },
 }));
