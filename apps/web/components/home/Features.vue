7a6a0286bffb1386a2f5edcf1e8ba4dce0fbd8b8
İsmail Emre Sırmalı <lynicis@protonmail.com>
2023-12-10 15:00:01 +0300

    chore: fix lint

--- apps/web/components/home/Features.vue
+++ apps/web/components/home/Features.vue
@@ -29,18 +29,25 @@ const features: FeatureProps[] = [
 
 <template>
   <div
-    class="bg-gradient-to-b from-transparent pb-36 md:pb-56 pt-48 md:pt-80 via-gray-100 to-gray-200 dark:via-gray-900 dark:to-gray-950 border-b-4 border-b-gray-50 dark:border-b-gray-900">
+    class="bg-gradient-to-b from-transparent pb-36 md:pb-56 pt-48 md:pt-80 via-gray-100 to-gray-200 dark:via-gray-900 dark:to-gray-950 border-b-4 border-b-gray-50 dark:border-b-gray-900"
+  >
     <UContainer>
       <h1
-        class="text-center text-4xl sm:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-500 to-gray-800 dark:from-gray-300 dark:to-gray-500 mb-3">
+        class="text-center text-4xl sm:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-500 to-gray-800 dark:from-gray-300 dark:to-gray-500 mb-3"
+      >
         {{ $t("home.features.title") }}
       </h1>
       <p class="text-center text-xl sm:text-2xl text-gray-500 mb-16">
         {{ $t("home.features.description") }}
       </p>
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 lg:gap-10">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 lg:gap-10"
+      >
         <UCard v-for="feature in features" :key="feature.title" :ui="ui">
-          <UIcon :name="feature.icon" class="text-5xl text-blue-400 dark:text-blue-500" />
+          <UIcon
+            :name="feature.icon"
+            class="text-5xl text-blue-400 dark:text-blue-500"
+          />
           <h1 class="mt-5 mb-2 text-2xl font-bold">
             {{ $t(feature.title) }}
           </h1>
