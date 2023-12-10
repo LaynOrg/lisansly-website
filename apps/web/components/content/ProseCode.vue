aad904bb54711085eb1c02a0dadd6ec2337df054
yali <tangozyusufali@gmail.com>
2023-12-11 02:08:17 +0300

    style(web): update prose code

---
+++ apps/web/components/content/ProseCode.vue
@@ -0,0 +1,19 @@
+<template>
+  <UCard class="rounded-xl rounded-t-none" :ui="{ body: { padding: '' } }">
+    <slot />
+  </UCard>
+</template>
+
+<style scoped>
+:slotted(pre) {
+  padding: 0.7rem;
+  counter-reset: lines;
+  text-align: left;
+}
+:slotted(.line::before) {
+  counter-increment: lines;
+  content: counter(lines);
+  margin-right: 1rem;
+  opacity: 0.3;
+}
+</style>
