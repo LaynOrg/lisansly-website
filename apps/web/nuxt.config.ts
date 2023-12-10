a28c36bcc8481235fd934e8542104f8ac564543b
yali <tangozyusufali@gmail.com>
2023-12-11 02:07:30 +0300

    style(web): add highlight theme

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -7,7 +7,16 @@ export default defineNuxtConfig({
     "@nuxtjs/i18n",
     "@nuxtjs/google-fonts",
     "@nuxtjs/eslint-module",
+    "@nuxt/content",
   ],
+  content: {
+    highlight: {
+      theme: {
+        default: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
   css: ["ui/css/main.css"],
   googleFonts: fontConfig,
   runtimeConfig: {
