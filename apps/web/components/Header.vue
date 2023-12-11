82442102434a21930ef7b1aaf887939929460107
yali <tangozyusufali@gmail.com>
2023-12-11 21:44:17 +0300

    feat(web): add shortcut and tooltip to toggle theme

--- apps/web/components/Header.vue
+++ apps/web/components/Header.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 import type { LinkProps } from "ui/types";
+
+const { metaSymbol } = useShortcuts();
+const isOpen = ref<boolean>(false);
 const colorMode = useColorMode();
+
+const links: LinkProps[] = [
+  { label: "header.links.0", href: "/" },
+  { label: "header.links.1", href: "/docs" },
+  { label: "header.links.2", href: "/pricing" },
+  { label: "header.links.3", href: "/dashboard" },
+];
 
 const isDark = computed({
   get() {
@@ -11,14 +21,14 @@ const isDark = computed({
   },
 });
 
-const links: LinkProps[] = [
-  { label: "header.links.0", href: "/" },
-  { label: "header.links.1", href: "/docs" },
-  { label: "header.links.2", href: "/pricing" },
-  { label: "header.links.3", href: "/dashboard" },
-];
-
-const isOpen = ref<boolean>(false);
+defineShortcuts({
+  meta_k: {
+    usingInput: true,
+    handler: () => {
+      isDark.value = !isDark.value;
+    },
+  },
+});
 </script>
 
 <template>
@@ -41,15 +51,23 @@ const isOpen = ref<boolean>(false);
       <div class="flex gap-3">
         <UButton icon="i-heroicons-user-circle-solid" color="gray" />
         <ClientOnly>
-          <UButton
-            :icon="
-              isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
-            "
-            color="gray"
-            variant="ghost"
-            aria-label="Theme"
-            @click="isDark = !isDark"
-          />
+          <UTooltip :ui="{ base: 'pb-6 px-1 gap-1 flex' }">
+            <UButton
+              :icon="
+                isDark
+                  ? 'i-heroicons-moon-20-solid'
+                  : 'i-heroicons-sun-20-solid'
+              "
+              color="gray"
+              variant="ghost"
+              aria-label="Theme"
+              @click="isDark = !isDark"
+            />
+            <template #text>
+              <UKbd :value="metaSymbol" />
+              <UKbd value="K" />
+            </template>
+          </UTooltip>
           <template #fallback>
             <div class="w-8 h-8" />
           </template>
