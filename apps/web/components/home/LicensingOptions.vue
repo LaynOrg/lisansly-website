7cf9c8c6b804fc5e1dfb04d50f58f60e5f0abd2a
yali <tangozyusufali@gmail.com>
2023-12-12 14:07:52 +0300

    style(web): update home licensing options section container max width

--- apps/web/components/home/LicensingOptions.vue
+++ apps/web/components/home/LicensingOptions.vue
@@ -28,7 +28,7 @@ const options: LicensingOptionProps[] = [
   <div
     class="bg-gradient-to-t from-transparent pt-36 md:pt-56 pb-48 md:pb-80 via-gray-100 to-gray-200 dark:via-gray-900 dark:to-gray-950 border-t-4 border-t-gray-50 dark:border-t-gray-900"
   >
-    <UContainer :ui="{ constrained: 'max-w-[90rem]' }">
+    <UContainer>
       <h1
         class="max-w-3xl py-2 m-auto text-center text-4xl sm:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-500 to-gray-800 dark:from-gray-300 dark:to-gray-500 mb-14"
       >
